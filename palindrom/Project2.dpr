6152e33772b0355a67a6bba25f1c90f5d8e40acf
TTPOrPAMMiCT <46588534+TTPOrPAMMiCT@users.noreply.github.com>
2019-01-18 16:33:06 +0300

    palindrom

--- palindrom/Project2.dpr
+++ palindrom/Project2.dpr
@@ -3,23 +3,22 @@ program Project2;
 {$APPTYPE CONSOLE}
 
 var start,endd : integer;
-    number : string;
-    i,flag : byte;
+    number,palindrome : string;
+    i : byte;
 
 begin
   readln(endd);
   for start := 1 to endd do
     begin
     str(start,number);
-    flag := 0;
+    palindrome := number;
     for i := 1 to length(number) div 2 do
       begin
         if number[length(number)-i+1] <> number[i]
         then
+        else
         begin
-          if flag = 0
-          then
-          writeln(number);
+          writeln(palindrome);
         end;
       end;
     end;
